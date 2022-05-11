f87d7473ff4d017a8c8512cdb228c9f7be8a246a
Felipe Silva <felipe.silva@cern.ch>
2022-05-11 17:21:21 -0400

    Working verison of nox.

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,125 @@
 from __future__ import annotations
 
+import os
+import shlex
 import shutil
+import sys
 from pathlib import Path
+from textwrap import dedent
 
 import nox
 
+try:
+    from nox_poetry import Session, session
+except ImportError:
+    message = f"""\
+    Nox failed to import the 'nox-poetry' package.
+    Please install it using the following command:
+    {sys.executable} -m pip install nox-poetry"""
+    raise SystemExit(dedent(message)) from None
+
 DIR = Path(__file__).parent.resolve()
 
-nox.options.sessions = ["black", "isort", "lint", "pylint", "mypy", "tests"]
+nox.options.sessions = ["black", "isort", "pre-commit", "pylint", "mypy", "tests"]
+
+python_versions = ["3.9"]
 
 
-@nox.session
-def lint(session: nox.Session) -> None:
+def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
+    """Activate virtualenv in hooks installed by pre-commit.
+    This function patches git hooks installed by pre-commit to activate the
+    session's virtual environment. This allows pre-commit to locate hooks in
+    that environment when invoked from git.
+    Args:
+        session: The Session object.
     """
-    Run the linter.
-    """
+    assert session.bin is not None
+
+    # Only patch hooks containing a reference to this session's bindir. Support
+    # quoting rules for Python and bash, but strip the outermost quotes so we
+    # can detect paths within the bindir, like <bindir>/python.
+    bindirs = [
+        bindir[1:-1] if bindir[0] in "'\"" else bindir
+        for bindir in (repr(session.bin), shlex.quote(session.bin))
+    ]
+
+    virtualenv = session.env.get("VIRTUAL_ENV")
+    if virtualenv is None:
+        return
+
+    headers = {
+        # pre-commit < 2.16.0
+        "python": f"""\
+            import os
+            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
+            os.environ["PATH"] = os.pathsep.join((
+                {session.bin!r},
+                os.environ.get("PATH", ""),
+            ))
+            """,
+        # pre-commit >= 2.16.0
+        "bash": f"""\
+            VIRTUAL_ENV={shlex.quote(virtualenv)}
+            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
+            """,
+        # pre-commit >= 2.17.0 on Windows forces sh shebang
+        "/bin/sh": f"""\
+            VIRTUAL_ENV={shlex.quote(virtualenv)}
+            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
+            """,
+    }
+
+    hookdir = Path(".git") / "hooks"
+    if not hookdir.is_dir():
+        return
+
+    for hook in hookdir.iterdir():
+        if hook.name.endswith(".sample") or not hook.is_file():
+            continue
+
+        if not hook.read_bytes().startswith(b"#!"):
+            continue
+
+        text = hook.read_text()
+
+        if not any(
+            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
+            for bindir in bindirs
+        ):
+            continue
+
+        lines = text.splitlines()
+
+        for executable, header in headers.items():
+            if executable in lines[0].lower():
+                lines.insert(1, dedent(header))
+                hook.write_text("\n".join(lines))
+                break
+
+
+@session(name="pre-commit", python=python_versions[0])
+def precommit(session: Session) -> None:
+    """Lint using pre-commit."""
+    args = session.posargs or [
+        "run",
+        "--all-files",
+        # "--hook-stage=manual",
+        # "--show-diff-on-failure",
+    ]
     session.install("pre-commit")
-    session.run("pre-commit", "run", "--all-files", *session.posargs)
+    session.run("pre-commit", *args)
+    activate_virtualenv_in_precommit_hooks(session)
+    # if args and args[0] == "install":
+    #     activate_virtualenv_in_precommit_hooks(session)
+
+
+# @nox.session
+# def lint(session: nox.Session) -> None:
+#     """
+#     Run the linter.
+#     """
+#     session.install("pre-commit")
+#     session.run("pre-commit", "run", "--all-files", *session.posargs)
 
 
 @nox.session
@@ -76,12 +179,19 @@ locations = "src", "tests", "noxfile.py"
 
 @nox.session
 def black(session: nox.Session) -> None:
-    """
-    Run Black.
-    """
+    """Run black code formatter."""
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+# min_cov = 98
+# @nox.session
+# def coverage(session: Session) -> None:
+#     """Upload coverage data."""
+#     install_with_constraints(session, "coverage[toml]", "codecov")
+#     session.run("coverage", "xml", f"--fail-under={min_cov}")
+#     session.run("codecov", *session.posargs)
 
 
 @nox.session
@@ -102,6 +212,7 @@ def mypy(session: nox.Session) -> None:
     """
     Run MyPy.
     """
+    session.run("poetry", "install", "--no-dev", external=True)
     args = session.posargs or mypy_locations
     session.install("mypy")
     session.run("mypy", *args)
--- src/hepdataframe/hepdataframe.py
+++ src/hepdataframe/hepdataframe.py
@@ -1,12 +1,30 @@
 """HEP Dataframe"""
-
-
 from __future__ import annotations
 
+# import awkward as ak
+import numpy as np
+from numpy.typing import ArrayLike
 
-def main(fooo) -> None:
+
+class HEPDataframe:
+    """HEP Dataframe"""
+
+    def __init__(self, data: ArrayLike) -> None:
+        """Define a HEPDataframe."""
+        # if not isinstance(data, ArrayLike):
+        #     raise TypeError("Events should be an 'awkward.Array'.")
+        self.data = data
+
+    @property
+    def my_property(self) -> ArrayLike:
+        """My property."""
+        return np.array([1, 2, 3])
+
+
+def main(fooo: str) -> None:
     """HEP Dataframe - Main"""
-    print(f"HEP Dataframe - {fooo}")
+    print(fooo)
+    print(np.array([1, 2, 3]))
 
 
 if __name__ == "__main__":
